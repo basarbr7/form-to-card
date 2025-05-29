bbc924b29dfc8506d6b919f70f1babc3cc867b91
M.A. Basar <basarbr7@gmail.com>
2025-05-29 13:27:44 +0600

    added toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
+
 import FormRoot from './page/formPage/FormRoot'
 
 
 const App = () => {
 
+
   return (
     <>
       <FormRoot />
+      
     </>
   )
 }
--- src/page/formPage/FormPage.jsx
+++ src/page/formPage/FormPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
 
 const FormPage = ({ handleFormData }) => {
     const [title, setTitle]= useState('')
@@ -10,24 +11,29 @@ const FormPage = ({ handleFormData }) => {
             handleFormData({title, desc})
             setTitle('')
             setDesc('')
+            toast("Added todo...",{
+                pauseOnHover: false,
+                theme: "dark"
+            })
         }
     };
   return (
     <div className='flex flex-col max-w-sm mx-auto justify-center items-center mt-10'>
         <h2 className='text-3xl mb-5 '>Collect data from a Form</h2>
-        <div className='w-full p-5 bg-green-500 flex flex-col gap-5'>
-            <form onSubmit={handleSubmit}>
+        <div className='w-full p-5 bg-green-500 '>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
                 <div className='flex flex-col gap-2'>
                     <label>Title:</label>
-                    <input type="text" name='title' value={title} className=' bg-white outline-none px-3 py-1' onChange={(e)=>setTitle(e.target.value)}/>
+                    <input type="text" name='title' value={title} className=' bg-white outline-none px-3 py-1' required onChange={(e)=>setTitle(e.target.value)}/>
                 </div>
                 <div className='flex flex-col gap-2'>
                     <label>Description:</label>
-                    <textarea type="text" name='desc' value={desc} className=' bg-white outline-none px-3 py-1 min-h-[100px] ' onChange={(e)=>setDesc(e.target.value)} />
+                    <textarea type="text" name='desc' value={desc} className=' bg-white outline-none px-3 py-1 min-h-[100px]' required onChange={(e)=>setDesc(e.target.value)} />
                 </div>
                 <button type='submit' className='bg-black text-white px-4 py-2 mt-3 cursor-pointer'>Send</button>
             </form>
             
+        <ToastContainer />
         </div>
     </div>
   )
